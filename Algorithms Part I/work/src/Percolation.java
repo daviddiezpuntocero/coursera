f9b8c4bac771c6316b645db6ba78d7f5b0ebebf6
Lawrence as Developer <smartlitchi@gmail.com>
2013-09-18 06:44:48 +0800

    Add stub for programing assignment 1

---
+++ Algorithms Part I/work/src/Percolation.java
@@ -0,0 +1,25 @@
+public class Percolation {
+    public  Percolation(int N) 
+    {
+    }
+
+    public boolean isOpen(int i, int j) 
+    {
+        return false;
+    }
+
+    public void open(int i, int j) 
+    {
+    }
+    
+    public boolean isFull(int i, int j) 
+    {
+        return false;
+    }
+    
+    public boolean percolates() 
+    {
+        return false;
+    }
+        
+}
